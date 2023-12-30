3278e4a896d3846ea531f5cb5f8bdbd337fba35d
saikumar <saikumar.rachakonda@exponentialai.com>
2023-12-31 00:50:25 +0530

    initial commit

---
+++ src/slices/Hero/Shapes.jsx
@@ -0,0 +1,140 @@
+"use client";
+
+import * as Three from "three";
+import { Canvas } from "@react-three/fiber";
+import { ContactShadows, Float, Environment } from "@react-three/drei";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+
+const Shapes = () => {
+  return (
+    <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0">
+      <Canvas
+        className="z-0"
+        shadows
+        gl={{ antialias: false }}
+        dpr={[1, 1.5]}
+        camera={{ position: [0, 0, 25], fov: 30, near: 1, far: 40 }}
+      >
+        <Suspense fallback={null}>
+          <Geometries />
+          <ContactShadows
+            position={[0, -3.5, 0]}
+            opacity={0.65}
+            scale={40}
+            blur={1}
+            far={9}
+          >
+            <Environment preset="studio" />
+          </ContactShadows>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
+
+export default Shapes;
+
+function Geometries() {
+  const geometries = [
+    {
+      position: [0, 0, 0],
+      r: 0.3,
+      geometry: new Three.IcosahedronGeometry(2.4),
+    },
+    {
+      position: [1, -0.75, 4],
+      r: 0.4,
+      geometry: new Three.CapsuleGeometry(0.5, 1.6, 2, 16),
+    },
+    {
+      position: [-1.4, 2, -4],
+      r: 0.4,
+      geometry: new Three.DodecahedronGeometry(1.5),
+    },
+    {
+      position: [-0.8, -0.75, 3],
+      r: 0.4,
+      geometry: new Three.OctahedronGeometry(1.5),
+    },
+    {
+      position: [1, 1, 4],
+      r: 0.4,
+      geometry: new Three.TorusGeometry(0.6, 0.25, 16, 32),
+    },
+  ];
+
+  const materials = [new Three.MeshNormalMaterial()];
+
+  return geometries.map(({ position, r, geometry }) => (
+    <Geometry
+      key={JSON.stringify(position)}
+      position={position.map((p) => p * 2)}
+      geometry={geometry}
+      materials={materials}
+      r={r}
+    />
+  ));
+}
+
+function Geometry({ r, position, geometry, materials }) {
+  const meshRef = useRef();
+  const [visible, setVisible] = useState(false);
+
+  const getRandomMaterial = () => {
+    return gsap.utils.random(materials);
+  };
+  const startingMaterial = getRandomMaterial();
+
+  const handleClick = (e) => {
+    const mesh = e.object;
+    gsap.to(mesh.rotation, {
+      x: `+=${gsap.utils.random(0, 2)}`,
+      y: `+=${gsap.utils.random(0, 2)}`,
+      z: `+=${gsap.utils.random(0, 2)}`,
+      duration: 1.3,
+      ease: "elastic.out(1,0.3)",
+      yoyo: true,
+    });
+    mesh.material = getRandomMaterial();
+  };
+
+  const handlePointerOver = () => {
+    document.body.style.cursor = "pointer";
+  };
+
+  const handlePointerOut = () => {
+    document.body.style.cursor = "default";
+  };
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      setVisible(true);
+      gsap.from(meshRef.current.scale, {
+        x: 0,
+        y: 0,
+        z: 0,
+        duration: 1,
+        ease: "elastic.out(1,0.3)",
+        delay: 0.1,
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <group position={position} ref={meshRef}>
+      <Float speed={5 * r} rotationIntensity={6 * r} floatIntensity={5 * r}>
+        <mesh
+          geometry={geometry}
+          onClick={handleClick}
+          onPointerOver={handlePointerOver}
+          onPointerOut={handlePointerOut}
+          visible={visible}
+          material={startingMaterial}
+        />
+      </Float>
+    </group>
+  );
+}
---
+++ src/slices/TechList/index.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useEffect, useRef } from "react";
+import { MdCircle } from "react-icons/md";
+
+gsap.registerPlugin(ScrollTrigger);
+
+/**
+ * Props for `TechList`.
+ */
+export type TechListProps = SliceComponentProps<Content.TechListSlice>;
+
+/**
+ * Component for "TechList" Slices.
+ */
+const TechList = ({ slice }: TechListProps): JSX.Element => {
+  const component = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: component.current,
+          markers: true,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 4,
+        },
+      });
+
+      tl.fromTo(
+        ".tech-row",
+        {
+          x: (index) => {
+            return index % 2 === 0
+              ? gsap.utils.random(600, 400)
+              : gsap.utils.random(-600, -400);
+          },
+        },
+        {
+          x: (index) => {
+            return index % 2 === 0
+              ? gsap.utils.random(-600, -400)
+              : gsap.utils.random(600, 400);
+          },
+        }
+      );
+    }, component);
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="wrapper overflow-hidden"
+      ref={component}
+    >
+      <Bounded as="div">
+        <Heading size="xl" className="mb-8" as="h2">
+          {slice.primary.heading}
+        </Heading>
+      </Bounded>
+      {slice.items.map(({ tech_color, tech_name }, index) => (
+        <div
+          key={index}
+          className="tech-row flex flex-row items-center justify-center mt-4 gap-4 text-slate-700"
+        >
+          {Array.from({ length: 15 }, (_, index) => (
+            <React.Fragment key={index}>
+              <span
+                style={{
+                  color: index % 3 === 0 && tech_color ? tech_color : "inherit",
+                }}
+                className="tech-item text-8xl font-extrabold uppercase tracking-tighter hover:text-yellow-300"
+              >
+                {tech_name}
+              </span>
+              <span className="text-3xl">
+                <MdCircle />
+              </span>
+            </React.Fragment>
+          ))}
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default TechList;
